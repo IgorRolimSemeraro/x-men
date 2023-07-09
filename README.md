a1421a368656ebfcd3bd96cd48b4757866ef33ad
IgorRolimSemeraro <irsemeraro@gmail.com>
2023-07-09 16:37:28 -0300

    postagem

---
+++ README.md
@@ -0,0 +1,21 @@
+# Calculadora de IMC
+
+# Olá, Esse é o meu projeto de seleção dos personagens de X-Men dos quadrinho. Esse projeto foi feito por mim durante as lives da "semana do zero ao programador contratado" do canal Dev em Dobro no YouTube. Foi usado e trabalhado meus conhecimentos de HTML, CSS e JavaScript.
+
+<!-- ![image](https://raw.githubusercontent.com/IgorRolimSemeraro/calculadora-de-imc/main/src/image/screenshot.png) -->
+
+## Ferramentas utilizadas:
+
+* HTML
+
+* CSS
+
+* Flex-box
+
+* JavaScript
+
+## Feito por:
+
+### Igor Rolim Semeraro
+
+### Linkedin: https://www.linkedin.com/in/igor-rolim-semeraro
---
+++ src/js/index.js
@@ -0,0 +1,43 @@
+// O que precisamos fazer? - quando passar o mouse em cima do personagem na lista temos que adicionar a borda azul de seleção na imagem pequena do personagem e mostrar a imagem, o nome e o texto grande do personagem que está selecionado
+
+const personagens = document.querySelectorAll('.personagem');
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+
+        if(window.innerWidth < 450) {
+            window.scrollTo({top:0, behavior: 'smooth' });
+        }
+
+        removerSelecaoDoPersonagem();
+
+        personagem.classList.add('selecionado');
+
+        alterarImagemPersonagemSelecionado(personagem);
+
+        alteraNomePersonagemSelecionado(personagem);
+
+        alterarDescricaoPersonagem(personagem);
+    })
+})
+
+function alterarDescricaoPersonagem(personagem) {
+    const descricaoPersonagem = document.getElementById('descricao-personagem');
+    descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+}
+
+function alteraNomePersonagemSelecionado(personagem) {
+    const nomePersonagem = document.getElementById('nome-personagem');
+    nomePersonagem.innerText = personagem.getAttribute('data-name');
+}
+
+function alterarImagemPersonagemSelecionado(personagem) {
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+    const idPersonagem = personagem.attributes.id.value;
+    imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+}
+
+function removerSelecaoDoPersonagem() {
+    const personagemSelecionado = document.querySelector('.selecionado');
+    personagemSelecionado.classList.remove('selecionado');
+}
